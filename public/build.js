fc50b589ebad448b04e0883f29323c83f489a73b
enlore <n.e.lorenson@gmail.com>
2016-09-12 17:36:25 -0400

    Tweaks

--- public/build.js
+++ public/build.js
@@ -1,14 +1,17 @@
 const app = new Vue({
     el: "#app",
+
     data: {
         photos: []
     },
+            console.log(photos)
     created () {
         fetch("/photos").then(resp => {
             return resp.json()
+
         }).then(photos => {
-            this.photos = photos
             console.log(photos)
+            this.photos = photos
         })
     }
 })
