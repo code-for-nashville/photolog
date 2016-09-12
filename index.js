3bbad9ea235c3a02aacf95c67f50c3f3556b0286
enlore <n.e.lorenson@gmail.com>
2016-09-12 17:53:03 -0400

    Superagent and req to photobomb svc

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const agent = require("superagent")
+
 const photos = "https://ga-bar-photobomb.herokuapp.com/"
 
 const express = require("express")
@@ -7,9 +9,16 @@ const app = express()
 app.use(express.static(__dirname + "/public"))
 
 app.get("/photos", (req, res) => {
-    return res.json([
-            { url: "woo", phoneNumber: "yeah" }
-    ])
+    agent.get(photos)
+        .set("accept", "application/json")
+        .end((err, resp) => {
+            if (err) res.json({ error: err })
+
+            else {
+                console.log(resp.body)
+                return resp.json(resp.body)
+            }
+        })
 })
 
 app.listen(process.env.PORT || 3000, () => {
